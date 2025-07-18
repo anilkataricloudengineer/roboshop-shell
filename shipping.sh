e9c1b9b00addddb1fe35d028a45830373fa92bc0
Anil Katari <anilkataricloudengineer@gmail.com>
2025-07-18 18:15:21 +0530

    Roboshop Project

--- shipping.sh
+++ shipping.sh
@@ -20,9 +20,9 @@ systemctl start shipping
 
 dnf install mysql -y
 
-mysql -h mysql-dev.anilcloudengineer -uroot -pRoboShop@1 < /app/db/schema.sql
-mysql -h mysql-dev.anilcloudengineer -uroot -pRoboShop@1 < /app/db/app-user.sql
-mysql -h mysql-dev.anilcloudengineer -uroot -pRoboShop@1 < /app/db/master-data.sql
+mysql -h mysql-dev.anilcloudengineer.online -uroot -pRoboShop@1 < /app/db/schema.sql
+mysql -h mysql-dev.anilcloudengineer.online -uroot -pRoboShop@1 < /app/db/app-user.sql
+mysql -h mysql-dev.anilcloudengineer.online -uroot -pRoboShop@1 < /app/db/master-data.sql
 
 systemctl restart shipping
 
