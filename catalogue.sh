342269b2e6dc75817d2c4612e7f625fd6bea4123
Anil Katari <anilkataricloudengineer@gmail.com>
2025-07-18 09:55:54 +0530

    Roboshop Project

--- catalogue.sh
+++ catalogue.sh
@@ -1,6 +1,8 @@
+cp catalogue.service /etc/systemd/system/catalogue.service
+cp mongo.repo /etc/yum.repos.d/mongo.repo
+
 dnf module disable nodejs -y
 dnf module enable nodejs:20 -y
-
 dnf install nodejs -y
 
 useradd roboshop
@@ -14,13 +16,9 @@ unzip /tmp/catalogue.zip
 cd /app
 npm install
 
-cp catalogue.service /etc/systemd/system/catalogue.service
-
 systemctl daemon-reload
 systemctl enable catalogue
 systemctl start catalogue
-
-cp mongo.repo /etc/yum.repos.d/mongo.repo
 
 dnf install mongodb-mongosh -y
 mongosh --host mongodb-dev.anilcloudengineer.online </app/db/master-data.js
