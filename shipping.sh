bc701d43b6358e126d93b6b4494fd74987a0cfec
Anil Katari <anilkataricloudengineer@gmail.com>
2025-08-02 21:10:42 +0530

    Roboshop Project

--- shipping.sh
+++ shipping.sh
@@ -8,9 +8,11 @@ dnf install mysql -y &>>$log_file
 Status_Print $?
 
 for file in schema app-user master-data; do
+
   echo Load $file
   mysql -h mysql-dev.anilcloudengineer.online -uroot -pRoboShop@1 < /app/db/$file.sql &>>$log_file
   Status_Print $?
+
 done
 
 
